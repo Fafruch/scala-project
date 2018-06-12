a8681e6ba9bf69eba1055af7640223059effb00d
Fafruch <fafruszek@gmail.com>
2018-06-13 00:59:56 +0200

    Refactoring
    NOTE: After refactoring there's one issue with rows: now row type is
    a key holding row's value, where before there wasn't any key,
    just an object

--- src/main/scala/com/example/app/MyScalatraServlet.scala
+++ src/main/scala/com/example/app/MyScalatraServlet.scala
@@ -1,5 +1,6 @@
 package com.example.app
 
+import scala.collection.mutable.ListBuffer
 import net.ruippeixotog.scalascraper.browser.JsoupBrowser
 import net.ruippeixotog.scalascraper.dsl.DSL._
 import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
@@ -8,29 +9,57 @@ import io.circe._
 import io.circe.generic.auto._
 import io.circe.parser._
 import io.circe.syntax._
-import scala.collection.mutable.ListBuffer
-
+import net.ruippeixotog.scalascraper.model.Element
 import org.scalatra._
 
-import scala.collection.mutable.ListBuffer
+
+sealed trait RowTrait
+case class RaceResult(grandPrix: String, date: String, winner: String, car: String, laps: Int, time: String) extends RowTrait
+case class DriverStanding(pos: Int, driver: String, nationality: String, car: String, pts: Int) extends RowTrait
+case class ErrorObject(status: String, title: String, details: Option[String])
 
 sealed trait ResponseTrait
-
-case class ErrorObject(status: String, title: String, details: Option[String])
+case class Response(data: ListBuffer[RowTrait] = ListBuffer()) extends ResponseTrait
 case class ErrorResponse(errors: ListBuffer[ErrorObject]) extends ResponseTrait
-
-case class RaceResult(grandPrix: String, date: String, winner: String, car: String, laps: Int, time: String)
-case class RacesResponse(data: ListBuffer[Option[RaceResult]] = ListBuffer()) extends ResponseTrait
-
-case class DriverStanding(pos: Int, driver: String, nationality: String, car: String, pts: Int)
-case class DriversResponse(data: ListBuffer[Option[DriverStanding]] = ListBuffer()) extends ResponseTrait
-
 
 object Response {
   def getURL(mode: String, year: Int): String = {
     val baseUrl = "https://www.formula1.com/en/results.html/"
 
     baseUrl + "/" + year + "/" + mode + ".html"
+  }
+
+  def getRows(rows: List[Element], mode: String): ListBuffer[RowTrait] = {
+    val listBuffer: ListBuffer[RowTrait] = ListBuffer()
+
+    rows.foreach { row =>
+      mode match {
+        case "races" =>
+          val tdList = row >> elementList("td")
+
+          val grandPrix = tdList(1) >> text("a")
+          val date = tdList(2).text
+          val winner = (tdList(3) >> elementList("span")).head.text + " " + (tdList(3) >> elementList("span")) (1).text
+          val car = tdList(4).text
+          val laps = tdList(5).text.toInt
+          val time = tdList(6).text
+
+          listBuffer += RaceResult(grandPrix, date, winner, car, laps, time)
+
+        case "drivers" =>
+          val tdList = row >> elementList("td")
+
+          val pos = tdList(1).text.toInt
+          val driver = (tdList(2) >> element("a") >> elementList("span")).head.text + " " + (tdList(2) >> elementList("span")) (1).text
+          val nationality = tdList(3).text
+          val car = tdList(4) >> text("a")
+          val pts = tdList(5).text.toInt
+
+          listBuffer += DriverStanding(pos, driver, nationality, car, pts)
+      }
+    }
+
+    listBuffer
   }
 
   def getResponse(mode: String, year: Int): String = {
@@ -42,48 +71,9 @@ object Response {
     val rows = doc >> element(".resultsarchive-table") >> elementList("tr")
     val rowsWithoutHeader = rows.drop(1)
 
-    val json = mode match {
-      case "races" =>
-        // this block needs refactoring
-        val racesResponse = RacesResponse()
+    val responseRows = getRows(rowsWithoutHeader, mode)
 
-        rowsWithoutHeader.foreach { row =>
-          val tdList = row >> elementList("td")
-
-          val grandPrix = tdList(1) >> text("a")
-          val date = tdList(2).text
-          val winner = (tdList(3) >> elementList("span")).head.text + " " + (tdList(3) >> elementList("span")) (1).text
-          val car = tdList(4).text
-          val laps = tdList(5).text.toInt
-          val time = tdList(6).text
-
-          val result = Some(RaceResult(grandPrix, date, winner, car, laps, time))
-          racesResponse.data += result
-        }
-
-        racesResponse.asJson // for more condensed response use ".noSpaces"
-
-      case "drivers" =>
-        // this block also needs refactoring
-        val driversResponse = DriversResponse()
-
-        rowsWithoutHeader.foreach { row =>
-          val tdList = row >> elementList("td")
-
-          val pos = tdList(1).text.toInt
-          val driver = (tdList(2) >> element("a") >> elementList("span")).head.text + " " + (tdList(2) >> elementList("span")) (1).text
-          val nationality = tdList(3).text
-          val car = tdList(4) >> text("a")
-          val pts = tdList(5).text.toInt
-
-          val standing = Some(DriverStanding(pos, driver, nationality, car, pts))
-          driversResponse.data += standing
-        }
-
-        driversResponse.asJson // for more condensed response use ".noSpaces"
-    }
-
-    json.toString
+    Response(responseRows).asJson.toString
   }
 
   def getErrorResponse(error: ErrorObject): String = {
