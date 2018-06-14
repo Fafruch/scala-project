6e171410fc36a449786ab3dcfcf7cbff1dba61d0
Fafruch <fafruszek@gmail.com>
2018-06-14 12:02:20 +0200

    Small refactoring in servlet

--- src/main/scala/com/example/app/MyScalatraServlet.scala
+++ src/main/scala/com/example/app/MyScalatraServlet.scala
@@ -8,12 +8,15 @@ class MyScalatraServlet extends ScalatraServlet {
     val mode: String = params("mode")
     val year: Int = params("year").toInt
 
-    if (year < 1950 || year > 2018) {
-      Response.getErrorResponse(ErrorObject("400", "Wrong year parameter", Some("Only 1950 to 2018 years are available")))
+    mode match {
+      case "races" | "drivers" | "team" | "fastest-laps" =>
+        if (year >= 1950 && year <= 2018)
+          Response.getResponse(mode, year)
+        else
+          Response.getErrorResponse(ErrorObject("400", "Wrong year parameter", Some("Only 1950 to 2018 years are available")))
 
-    } else mode match {
-        case "races" | "drivers" | "team" | "fastest-laps" => Response.getResponse(mode, year)
-        case _ => Response.getErrorResponse(ErrorObject("404", "No route for '/" + mode + "'", None))
-      }
+      case _ =>
+        Response.getErrorResponse(ErrorObject("404", "No route for '/" + mode + "'", None))
+    }
   }
 }
